290f3115687cc6d3a722eebdf46abef96f22d9a5
EfeAdzhar <efeadzhar@gmail.com>
2021-11-27 14:06:25 +0200

    Now you can add Items in ToDo ;)

--- ToDo/ToDoList.swift
+++ ToDo/ToDoList.swift
@@ -6,27 +6,20 @@ import UIKit
 class ToDoList: UIViewController {
     //MARK: Initialization
     @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addButton: UIButton!
-    //    var toDO = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    var toDO : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    //MARK: Add Items
+    func addItems(nameItem : String) {
+        toDO.append(nameItem)
         
     }
-    //    func  numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    //        return 1
-    //    }
-    //
-    //    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //            return toDO.count
-    //        }
-    //    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    //        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as UITableViewCell
-    //            cell.textLabel!.text = toDO[indexPath.row]
-    //            return cell
-    //        }
-    //
+    
     //MARK: Showing when View will Appear
     override func viewDidAppear(_ animated: Bool) {
         welcomeAlert(title: "Welcome to ToDoList", message: "It's still in development", style: .alert)
@@ -55,10 +48,29 @@ class ToDoList: UIViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let alertButton1 = UIAlertAction(title: "Cancel", style: .cancel, handler: .none)
         let alertButton2 = UIAlertAction(title: "Add", style: .default) { UITableView in
+            let newItem = alert.textFields?[0].text
+            self.addItems(nameItem: newItem!)
+            self.tableView.reloadData()
         }
         alert.addTextField(configurationHandler: { textField in textField.placeholder = "New Task"})
         alert.addAction(alertButton1)
         alert.addAction(alertButton2)
         self.present(alert, animated: true, completion: .none)
+    }
+}
+//MARK: Table View Data Source and Delegate
+extension ToDoList : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDO.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = toDO[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
